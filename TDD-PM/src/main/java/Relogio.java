8634f1f09de003b439bc696ecb9d06c917d6ffac
Igor M. Santos <igormsprofissional@gmail.com>
2023-08-30 21:35:39 -0300

    Test 003 TEST ERROR2

--- TDD-PM/src/main/java/Relogio.java
+++ TDD-PM/src/main/java/Relogio.java
@@ -18,5 +18,35 @@ public class Relogio {
         this.minutos = 0;
         this.segundos = 0;
     }
+    public void marcarInicioIntervalo() {
+        horaInicioIntervalo = horas;
+        minutoInicioIntervalo = minutos;
+        segundoInicioIntervalo = segundos;
+    }
 
+    public void marcarFimIntervalo() {
+        int horasDecorridas = horas - horaInicioIntervalo;
+        int minutosDecorridos = minutos - minutoInicioIntervalo;
+        int segundosDecorridos = segundos - segundoInicioIntervalo;
+
+        horas = minutos = segundos = 0; // Reiniciar o relógio
+
+        // Ajustar para lidar com subtrações negativas
+        if (segundosDecorridos < 0) {
+            segundosDecorridos += 60;
+            minutosDecorridos--;
+        }
+        if (minutosDecorridos < 0) {
+            minutosDecorridos += 60;
+            horasDecorridas--;
+        }
+        if (horasDecorridas < 0) {
+            horasDecorridas += 24; // Lidar com o caso de passar da meia-noite
+        }
+
+        horas = horasDecorridas;
+        minutos = minutosDecorridos;
+        segundos = segundosDecorridos;
+
+    }
 }
